1792fccadd0ef9585886fa4ba830cdabe02cd757
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-07-15 03:01:02 +0530

    Add : Creating addressbook table in addressbook_service_db database

--- SQL_Queries/AddressBookSystemSqlQueries.sql
+++ SQL_Queries/AddressBookSystemSqlQueries.sql
@@ -1,2 +1,16 @@
 create database addressbook_service_db;
 show databases;
+
+use addressbook_service_db;
+create table AddressBook (
+     FirstName varchar(25) not null,
+     LastName varchar(25) not null,
+     Address varchar(150) not null,
+     City varchar(25) not null,
+     State varchar(25) not null,
+     Zip int not null,
+     PhoneNumber int not null,
+     Email varchar(50) not null
+     );
+show tables;
+describe addressbook;     
