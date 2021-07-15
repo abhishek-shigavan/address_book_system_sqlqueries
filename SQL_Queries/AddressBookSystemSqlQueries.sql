a01ab3a1b436703e428536640138d5cd50f5d381
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-07-15 23:17:36 +0530

    Refactor: Getting no of contact person by their ContactType

--- SQL_Queries/AddressBookSystemSqlQueries.sql
+++ SQL_Queries/AddressBookSystemSqlQueries.sql
@@ -1,24 +1,24 @@
 # UC1
-create database addressbook_service_db;  # Creating database
-show databases;
+create database addressbook_service_db; 				# Creating database
+show databases;								# show all databases
 
 # UC2
 use addressbook_service_db;
-create table AddressBook (
-     FirstName varchar(25) not null,
-     LastName varchar(25) not null,
-     Address varchar(150) not null,
-     City varchar(25) not null,
-     State varchar(25) not null,
-     Zip int not null,
-     PhoneNumber bigint not null,
-     Email varchar(50) not null
+create table AddressBook (						# creating table
+     FirstName varchar(25) not null,					# first name of person
+     LastName varchar(25) not null,					# last name of person
+     Address varchar(150) not null,					# address of person
+     City varchar(25) not null,						# city name of person
+     State varchar(25) not null,					# state name of person
+     Zip int not null,							# zip code of person
+     PhoneNumber bigint not null,					# phone number of person
+     Email varchar(50) not null						# email id of person
      );
-show tables;
-describe addressbook;  
+show tables;								# show all present tables
+describe addressbook;  							# displaying structure of table
 
 # UC3
-insert into addressbook ( 
+insert into addressbook ( 						# inserting data into table
 	FirstName, LastName, Address, City,
     	State, Zip, PhoneNumber, Email 
     	)
@@ -29,33 +29,34 @@ insert into addressbook (
 	  'Maharashtra', '400002', '7898676767', 'abhi@gmail.com' ),
     	( 'Omkar', 'Pqr', 'R.No 505 Ratnagiri Soc.', 'Ranchi',
           'Jharkhand', '2001243', '7678767890', 'om@gmail.com' );
-select * from addressbook;
-
+select * from addressbook;						# retrieving all data of table 
+ 
 # UC4
-select * from addressbook;
-update addressbook 
+select * from addressbook;							# displaying all data of table
+update addressbook 								# updating data of contact
 	set LastName = 'Singh', Zip = '200123', Email = 'omkar@gmail.com' 
 	where FirstName = 'Omkar';
-select * from addressbook; 
+select * from addressbook;    						        # displaying all data of table
 
 # UC5
 select * from addressbook;
-delete from addressbook where FirstName = 'Akshay';
+delete from addressbook where FirstName = 'Akshay';			# deleting entry of data from table
 select * from addressbook;
 
 # UC6
-select * from addressbook where City = 'Ranchi';
-select * from addressbook where State = 'Maharashtra';
+select * from addressbook where City = 'Ranchi';			# retrieving data of city
+select * from addressbook where State = 'Maharashtra';			# retrieving data of state
 
 # UC7
-select count(City) from addressbook;
-select count(State) from addressbook;
+select count(City) from addressbook;				# retrieving count of city enteries in table
+select count(State) from addressbook;				# retrieving count of state enteries in table
 
 # UC8
 select * from addressbook;
-select * from addressbook where City = 'Pune' order by FirstName;
+select * from addressbook where City = 'Pune' order by FirstName;   # retrieving data of city in sorted order
 
 # UC9
+# adding new data fields into table 
 alter table addressbook add AddressBookName varchar(40) not null default 'N/A' first;
 alter table addressbook add ContactType varchar(40) default 'N/A' after AddressBookName; 
 
@@ -66,8 +67,7 @@ update addressbook set AddressBookName = 'Favourites', ContactType = 'Family' wh
 update addressbook set AddressBookName = 'Favourites', ContactType = 'Family' where FirstName = 'Omkar';
 update addressbook set AddressBookName = 'Important', ContactType = 'Profession' where FirstName = 'Bala';
 update addressbook set AddressBookName = 'Important', ContactType = 'Profession' where FirstName = 'Teja';
-
 #getting count of each ContactType
 select count(ContactType) from addressbook where ContactType = 'Family';
 select count(ContactType) from addressbook where ContactType = 'Friend';
-select count(ContactType) from addressbook where ContactType = 'Profession';   
+select count(ContactType) from addressbook where ContactType = 'Profession';
